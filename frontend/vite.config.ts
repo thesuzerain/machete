940906e4ac189e1ebebf93d0c254393dc23388fc
thesuzerain <wverchere@gmail.com>
2024-11-19 22:15:54 -0800

    feat: added env variable

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+
+const env = loadEnv(process.env.NODE_ENV as string, process.cwd());
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		proxy: {
 			'/api': {
-				target: 'http://localhost:8000',
+				target: env.PUBLIC_API_URL ?? 'http://localhost:8000',
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, '')
 			}
